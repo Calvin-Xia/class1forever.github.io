b978f983a97e4679bd90e46c0ed5498aac415fad
Calvin-Xia <2383451069@qq.com>
2025-09-08 11:13:17 +0800

    Update data.js
    按照行政区划代码、城市、学校名称进行排列

--- js/data.js
+++ js/data.js
@@ -1,8 +1,38 @@
 let students=
 [
     {
+        "name": "程语晗",
+        "school": "北京电子科技学院",
+        "city": "丰台区",
+        "province": "北京"
+    },
+    {
+        "name": "管健宇",
+        "school": "对外经济贸易大学",
+        "city": "朝阳区",
+        "province": "北京"
+    },
+    {
         "name": "陈彦哲",
         "school": "北京邮电大学",
+        "city": "昌平区",
+        "province": "北京"
+    },
+    {
+        "name": "黄昶睿",
+        "school": "中国政法大学",
+        "city": "昌平区",
+        "province": "北京"
+    },
+    {
+        "name": "刘镇蓝",
+        "school": "北京航空航天大学",
+        "city": "昌平区",
+        "province": "北京"
+    },
+    {
+        "name": "张钧然",
+        "school": "华北电力大学",
         "city": "昌平区",
         "province": "北京"
     },
@@ -25,10 +55,46 @@ let students=
         "province": "北京"
     },
     {
-        "name": "刘镓宇",
-        "school": "浙江大学",
-        "city": "杭州",
-        "province": "浙江"
+        "name": "明睿祺",
+        "school": "北京理工大学",
+        "city": "海淀区",
+        "province": "北京"
+    },
+    {
+        "name": "徐旭",
+        "school": "北京交通大学",
+        "city": "海淀区",
+        "province": "北京"
+    },
+    {
+        "name": "胡皓然",
+        "school": "吉林大学",
+        "city": "长春",
+        "province": "吉林"
+    },
+    {
+        "name": "钟佳芮",
+        "school": "吉林大学",
+        "city": "长春",
+        "province": "吉林"
+    },
+    {
+        "name": "冉昊民",
+        "school": "哈尔滨工业大学",
+        "city": "哈尔滨",
+        "province": "黑龙江"
+    },
+    {
+        "name": "陈星宇",
+        "school": "上海交通大学",
+        "city": "闵行区",
+        "province": "上海"
+    },
+    {
+        "name": "文宇昊",
+        "school": "上海交通大学",
+        "city": "闵行区",
+        "province": "上海"
     },
     {
         "name": "喻梓珂",
@@ -37,19 +103,79 @@ let students=
         "province": "上海"
     },
     {
-        "name": "李天宇",
-        "school": "电子科技大学",
-        "city": "成都",
-        "province": "四川"
+        "name": "卢雨霏",
+        "school": "复旦大学",
+        "city": "杨浦区",
+        "province": "上海"
     },
     {
-        "name": "但佳阳",
-        "school": "电子科技大学",
-        "city": "成都",
-        "province": "四川"
+        "name": "周琳奇",
+        "school": "复旦大学",
+        "city": "杨浦区",
+        "province": "上海"
+    },
+    {
+        "name": "张俊淘",
+        "school": "同济大学",
+        "city": "杨浦区",
+        "province": "上海"
+    },
+    {
+        "name": "刘镓宇",
+        "school": "浙江大学",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "付翔语",
+        "school": "浙江大学",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "张钧豪",
+        "school": "浙江大学",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "周金河",
+        "school": "南京理工大学",
+        "city": "南京",
+        "province": "江苏"
+    },
+    {
+        "name": "李泽熙",
+        "school": "中国科学技术大学",
+        "city": "合肥",
+        "province": "安徽"
+    },
+    {
+        "name": "李晨曦",
+        "school": "山东大学",
+        "city": "威海",
+        "province": "山东"
+    },
+    {
+        "name": "黄旭",
+        "school": "华中科技大学",
+        "city": "武汉",
+        "province": "湖北"
+    },
+    {
+        "name": "刘体豪",
+        "school": "华中科技大学",
+        "city": "武汉",
+        "province": "湖北"
     },
     {
         "name": "沈浩轩",
+        "school": "华中科技大学",
+        "city": "武汉",
+        "province": "湖北"
+    },
+    {
+        "name": "牟炳燚",
         "school": "华中科技大学",
         "city": "武汉",
         "province": "湖北"
@@ -67,142 +193,10 @@ let students=
         "province": "广东"
     },
     {
-        "name": "刘体豪",
-        "school": "华中科技大学",
-        "city": "武汉",
-        "province": "湖北"
-    },
-    {
-        "name": "明睿祺",
-        "school": "北京理工大学",
-        "city": "海淀区",
-        "province": "北京"
-    },
-    {
-        "name": "张俊淘",
-        "school": "同济大学",
-        "city": "杨浦区",
-        "province": "上海"
-    },
-    {
-        "name": "黄昶睿",
-        "school": "中国政法大学",
-        "city": "昌平区",
-        "province": "北京"
-    },
-    {
-        "name": "周琳奇",
-        "school": "复旦大学",
-        "city": "杨浦区",
-        "province": "上海"
-    },
-    {
-        "name": "程语晗",
-        "school": "北京电子科技学院",
-        "city": "丰台区",
-        "province": "北京"
-    },
-    {
-        "name": "黄旭",
-        "school": "华中科技大学",
-        "city": "武汉",
-        "province": "湖北"
-    },
-    {
-        "name": "陈立业",
-        "school": "重庆大学",
-        "city": "沙坪坝区",
-        "province": "重庆"
-    },
-    {
-        "name": "罗彩月",
-        "school": "西安交通大学",
-        "city": "西安",
-        "province": "陕西"
-    },
-    {
-        "name": "李雨辰",
-        "school": "重庆大学",
-        "city": "沙坪坝区",
-        "province": "重庆"
-    },
-    {
-        "name": "牟炳燚",
-        "school": "华中科技大学",
-        "city": "武汉",
-        "province": "湖北"
-    },
-    {
-        "name": "管健宇",
-        "school": "对外经济贸易大学",
-        "city": "朝阳区",
-        "province": "北京"
-    },
-    {
-        "name": "李晨曦",
-        "school": "山东大学",
-        "city": "威海",
-        "province": "山东"
-    },
-    {
         "name": "冉又丹",
         "school": "西南大学",
         "city": "北碚区",
         "province": "重庆"
-    },
-    {
-        "name": "冉昊民",
-        "school": "哈尔滨工业大学",
-        "city": "哈尔滨",
-        "province": "黑龙江"
-    },
-    {
-        "name": "张钧豪",
-        "school": "浙江大学",
-        "city": "杭州",
-        "province": "浙江"
-    },
-    {
-        "name": "徐旭",
-        "school": "北京交通大学",
-        "city": "海淀区",
-        "province": "北京"
-    },
-    {
-        "name": "周金河",
-        "school": "南京理工大学",
-        "city": "南京",
-        "province": "江苏"
-    },
-    {
-        "name": "胡皓然",
-        "school": "吉林大学",
-        "city": "长春",
-        "province": "吉林"
-    },
-    {
-        "name": "李凯阳",
-        "school": "电子科技大学",
-        "city": "成都",
-        "province": "四川"
-    },
-    {
-        "name": "钟佳芮",
-        "school": "吉林大学",
-        "city": "长春",
-        "province": "吉林"
-    },
-    {
-        "name": "李怡霏",
-        "school": "重庆大学",
-        "city": "沙坪坝区",
-        "province": "重庆"
-    },
-    {
-        "name": "陈星宇",
-        "school": "上海交通大学",
-        "city": "闵行区",
-        "province": "上海"
     },
     {
         "name": "汪柏承",
@@ -211,49 +205,31 @@ let students=
         "province": "重庆"
     },
     {
-        "name": "刘镇蓝",
-        "school": "北京航空航天大学",
-        "city": "昌平区",
-        "province": "北京"
-    },
-    {
-        "name": "牟佳成",
+        "name": "陈立业",
         "school": "重庆大学",
         "city": "沙坪坝区",
         "province": "重庆"
     },
     {
-        "name": "文宇昊",
-        "school": "上海交通大学",
-        "city": "闵行区",
-        "province": "上海"
-    },
-    {
-        "name": "李泽熙",
-        "school": "中国科学技术大学",
-        "city": "合肥",
-        "province": "安徽"
-    },
-    {
-        "name": "卢雨霏",
-        "school": "复旦大学",
-        "city": "杨浦区",
-        "province": "上海"
-    },
-    {
-        "name": "付翔语",
-        "school": "浙江大学",
-        "city": "杭州",
-        "province": "浙江"
-    },
-    {
-        "name": "张钧然",
-        "school": "华北电力大学",
-        "city": "昌平区",
-        "province": "北京"
-    },
-    {
         "name": "冯尧",
+        "school": "重庆大学",
+        "city": "沙坪坝区",
+        "province": "重庆"
+    },
+    {
+        "name": "李怡霏",
+        "school": "重庆大学",
+        "city": "沙坪坝区",
+        "province": "重庆"
+    },
+    {
+        "name": "李雨辰",
+        "school": "重庆大学",
+        "city": "沙坪坝区",
+        "province": "重庆"
+    },
+    {
+        "name": "牟佳成",
         "school": "重庆大学",
         "city": "沙坪坝区",
         "province": "重庆"
@@ -263,6 +239,29 @@ let students=
         "school": "重庆大学",
         "city": "沙坪坝区",
         "province": "重庆"
+    },
+    {
+        "name": "李天宇",
+        "school": "电子科技大学",
+        "city": "成都",
+        "province": "四川"
+    },
+    {
+        "name": "但佳阳",
+        "school": "电子科技大学",
+        "city": "成都",
+        "province": "四川"
+    },
+    {
+        "name": "李凯阳",
+        "school": "电子科技大学",
+        "city": "成都",
+        "province": "四川"
+    },
+    {
+        "name": "罗彩月",
+        "school": "西安交通大学",
+        "city": "西安",
+        "province": "陕西"
     }
-    
 ]
